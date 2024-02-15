781cd8c22a9da235340198ce67aa070f57ad535c
Abdussamet ERSOYLU <abdussametersoylu@gmail.com>
2024-02-15 04:03:05 +0300

    reached a dead end the program works in a flawed system right now. initializer always sets, the new item to function, tho it should set new ones. so it works for only one object for each type. ill try to figure out to overcome this problem

--- SysArr.h
+++ SysArr.h
@@ -11,17 +11,20 @@
 
 #define Define_Array_Struct(type, type_name) \
         struct array_##type_name { \
+            /* Member Functions */\
             type * array; \
             size_t size; \
             size_t capacity; \
+            /* Function pointers;  */\
             type (*GetValue)(size_t); \
             void (*SetValue)(size_t, type); \
+            void (*Resize)(size_t);\
         }; \
         typedef struct array_##type_name array_##type_name;
 
 
 #define Define_Array_Default_Get_Function(type, name, type_name) \
-        type name(size_t index, array_##type_name * this) { \
+        type  name(size_t index, array_##type_name * this) { \
             static array_##type_name * _this = NULL;  \
             if(this != NULL) { \
                 _this = this;  \
@@ -37,7 +40,7 @@
 
 #define Define_Array_Get_Function(type, name, type_name) \
         Define_Array_Default_Get_Function(type, MACRO_CONCAT(name, _def), type_name)\
-        type name(size_t index) {\
+        type  name(size_t index) {\
             return MACRO_CONCAT(name, _def)(index, NULL);\
         }
 
@@ -61,23 +64,31 @@
             MACRO_CONCAT(name, _def)(index, value, NULL);\
         }
 
+#define Define_Array_Resize_Function(type, name, type_name) \
 
-#define CREATE_INITIALIZER(type, set, get, type_name) \
-        Define_Array_Struct(type, type_name) \
-        Define_Array_Set_Function(type, set, type_name) \
-        Define_Array_Get_Function(type, get, type_name) \
-        void MACRO_CONCAT(init_, array_##type_name)(array_##type_name * item, size_t size) {\
+#define INITIALIZER(type, initializer_name, set_name, get_name, type_name)\
+        void initializer_name(array_##type_name * item, size_t size) {\
             item->array = (type*)malloc(sizeof(type) * size);\
-            MACRO_CONCAT(get, _def)(0, item);\
-            MACRO_CONCAT(set, _def)(0, 0, item);\
-            item->GetValue = get;\
-            item->SetValue = set;\
+            MACRO_CONCAT(get_name, _def)(0, item);\
+            MACRO_CONCAT(set_name, _def)(0, 0, item);\
+            item->GetValue = get_name;\
+            item->SetValue = set_name;\
             item->size = size;\
             item->capacity = size; \
         }
 
+#define CREATE_INITIALIZER(type, initializer_name, set_name, get_name, type_name) \
+        Define_Array_Set_Function(type, set_name, type_name) \
+        Define_Array_Get_Function(type, get_name, type_name) \
+        INITIALIZER(type, initializer_name, set_name, get_name, type_name)\
+        void (*init_array_##type_name)(array_##type_name * item, size_t size) = initializer_name; 
+
+#define RECREATE_INITIALIZER(type, type_name) \
+        CREATE_INITIALIZER(type, CREATE_FUNCTION_NAME(type_name, init), CREATE_FUNCTION_NAME(type, set), CREATE_FUNCTION_NAME(type, get), type_name)
+
 #define CREATE_ARRAY_TYPE(type, type_name) \
-CREATE_INITIALIZER(type, CREATE_FUNCTION_NAME(type_name, set), CREATE_FUNCTION_NAME(type_name, get), type_name) 
+        Define_Array_Struct(type, type_name) \
+        CREATE_INITIALIZER(type, CREATE_FUNCTION_NAME(type_name, init), CREATE_FUNCTION_NAME(type, set), CREATE_FUNCTION_NAME(type, get), type_name)
 
 
 #define CREATE_ARRAY_TYPES \
@@ -96,7 +107,9 @@ CREATE_INITIALIZER(type, CREATE_FUNCTION_NAME(type_name, set), CREATE_FUNCTION_N
     CREATE_ARRAY_TYPE(size_t, size_t) \
 
 
-CREATE_ARRAY_TYPES
+//CREATE_ARRAY_TYPES
+
+CREATE_ARRAY_TYPE(int, int);
 
 
 
--- main.c
+++ main.c
@@ -1,11 +1,28 @@
-#include "SysArr.h"
-#include <unistd.h>
 #include "SysIO.h"
+#include "SysArr.h"
 
-int main() {
+void Resize_Impl(size_t size, array_int * this) {
+    static array_int * _this; 
+    if(this != NULL) {
+        _this = this; 
+    }
+    if(_this == NULL) {
+        return; 
+    }
+    _this->array = (int*)realloc(_this->array, size); 
+}
+
+void Resize(size_t size) {
+    Resize_Impl(size, NULL); 
+}
+
+
+int main(void) {
+    _print_f("Hello World!\n"); 
     array_int arr; 
     init_array_int(&arr, 10); 
     arr.SetValue(0, 10);
-    _print_f("%d\n", arr.GetValue(0));
+
+    _write(arr.GetValue(0)); 
     return 0; 
 }
